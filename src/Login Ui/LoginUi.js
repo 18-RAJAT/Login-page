4d407286c183e084c577157a4b618b41b3f9d582
Rajat <Rajatjoshi7535@gamil.com>
2022-03-17 15:15:51 +0530

    Initial-commit

---
+++ src/Login Ui/LoginUi.js
@@ -0,0 +1,46 @@
+import './LoginUi.css';
+import profile from "./../image/a.png";
+import email from "./../image/email.png";
+import pass from "./../image/pass.png";
+function LoginUi() {
+  return (
+    <div className="main">
+     <div className="sub-main">
+       <div>
+         <div className="imgs">
+           <div className="container-image">
+             <img src={profile} alt="profile" className="profile"/>
+
+           </div>
+
+
+         </div>
+         <div>
+           <h1>Login Page</h1>
+           <div>
+             <img src={email} alt="email" className="email"/>
+             <input type="text" placeholder="user name" className="name"/>
+           </div>
+           <div className="second-input">
+             <img src={pass} alt="pass" className="email"/>
+             <input type="password" placeholder="user name" className="name"/>
+           </div>
+          <div className="login-button">
+          <button>Login</button>
+          </div>
+           
+            <p className="link">
+              <a href="#"> Forgot password ? </a> or <a href="#"> Sign Up </a>
+            </p>
+           
+ 
+         </div>
+       </div>
+       
+
+     </div>
+    </div>
+  );
+}
+
+export default LoginUi;
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css'; 
+import LoginUi from './Login Ui/LoginUi';
+
+ReactDOM.render( 
+    <LoginUi /> ,
+  document.getElementById('root')
+);
